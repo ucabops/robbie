ba57cda7513790d5b2065bd32a6f4f1b46886ca0
Jack Copsey <jack.copsey.19@ucl.ac.uk>
2020-03-28 19:58:40 +0000

    Create special subset of 10 xws

---
+++ source/scrape/specialscrape.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+
+from quickscrape import scrape
+
+
+if __name__ == '__main__':
+    script_desc = 'Download a subset of quick crosswords from the Guardian\'s website.'
+    parser = argparse.ArgumentParser(description=script_desc)
+    args = parser.parse_args()
+    
+    # Specify list of crosswords to download
+    xw_list = [20, 14, 487, 318, 408, 39, 190, 90, 250, 426]
+    num_xws = len(xw_list)
+
+    # Scrape data
+    xwset = {}
+    for i, xw in enumerate(xw_list, start=1):
+        xw += 10000
+        print(f'Downloading crossword {i} of {num_xws} (#{xw})...')
+        xwset[str(xw)] = scrape(xw)
+    
+    # Save results
+    out_path = f'./data/raw/gquick-{num_xws}.json'
+    with open(out_path, 'w+') as file:
+        json.dump(xwset, file)
+    
+    print(f"Dataset saved to {out_path}")
